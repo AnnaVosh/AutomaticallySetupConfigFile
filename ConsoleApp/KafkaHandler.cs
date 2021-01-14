4374aca3b13bc96382e5d3153ff523cc47410b25
Коптева Анна Витальевна <avkopte1@mts.ru>
2021-01-14 23:42:41 +0300

    First commit

---
+++ ConsoleApp/KafkaHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomerHub.SubscriberCDP
+{
+    public class KafkaHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _eventMainTopic;
+        private readonly string _eventDelayTopic;
+        private readonly string _eventRejectTopic;
+
+        public KafkaHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _eventMainTopic = _configuration.GetSection("Kafka").GetValue<string>("EventMainTopicTitle");
+            _eventDelayTopic = _configuration.GetSection("Kafka").GetValue<string>("EventDelayTopicTitle");
+            _eventRejectTopic = _configuration.GetSection("Kafka").GetValue<string>("EventRejectTopicTitle");
+        }
+        public Task Start()
+        {
+            SetupConsumer();           
+            return Task.CompletedTask;
+        }
+
+        private void SetupConsumer()
+        {
+            Console.WriteLine($"Main topic: {_eventMainTopic}");
+            Console.WriteLine($"Delay topic: {_eventDelayTopic}");
+            Console.WriteLine($"Reject topic: {_eventRejectTopic}");
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerHub.SubscriberCDP
+{
+    public class Program
+    {
+        private static IConfiguration _configuration;
+
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = ConfigureServices();
+            {
+                await serviceProvider
+                    .GetService<KafkaHandler>()
+                    .Start();
+            }
+        }
+
+        /// <summary>
+        /// Конфигурация сервиса
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            const string environmentVariableName = "ASPNETCORE_ENVIRONMENT";
+            var environmentName =
+                //Environment.GetEnvironmentVariable(environmentVariableName, EnvironmentVariableTarget.Machine) ??
+                Environment.GetEnvironmentVariable(environmentVariableName);
+
+            var services = new ServiceCollection();
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json")
+                .AddEnvironmentVariables()
+                .Build();
+            services.AddSingleton(_configuration);
+            services.AddSingleton<KafkaHandler>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
